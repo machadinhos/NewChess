27208c692aa6f1f39916e4db27f5e41966d4ea27
filipe-silva-27 <filipesprogramming@gmail.com>
2023-06-28 18:32:59 +0100

    feat: abstract piece

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,5 +1,31 @@
 package pieces;
 
-public class Piece {
+import java.awt.*;
+import java.util.List;
+
+public abstract class Piece {
+
+	private Point position;
+
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public void setPosition(final Point position) {
+		this.position = position;
+	}
+
+	public int getX() {
+		return (int) position.getX();
+	}
+
+	public int getY() {
+		return (int) position.getY();
+	}
+
+	public abstract void move();
+	public abstract List<Point> getValidMoves();
+	public abstract boolean checkValidMove();
 
 }
