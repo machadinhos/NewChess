9451536c8dd2c075d8d4b37571cf5dda5aff0bc8
machadinhos <pbmachado2002@gmail.com>
2023-06-29 19:28:23 +0100

    Coded dional moves

--- src/pieces/PiecesUtils.java
+++ src/pieces/PiecesUtils.java
@@ -6,18 +6,6 @@ import java.util.List;
 
 public class PiecesUtils {
 	
-	public static boolean isKingSafe (final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
-		
-		for (final Piece adversaryPiece : adversaryPieces) {
-			if (adversaryPiece.checkValidMove(kingPosition, board)) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	
 	public static List<Point> getDiagonalUpRightMoves (Piece piece, final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
 		
 		final List<Point> moves = new ArrayList<>();
@@ -65,6 +53,132 @@ public class PiecesUtils {
 		}
 		
 		return boardCopy;
+	}
+	
+	
+	public static List<Point> getDiagonalUpLeftMoves (Piece piece, final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
+		
+		final List<Point> moves = new ArrayList<>();
+		
+		int col = piece.getCol();
+		int row = piece.getRow();
+		
+		Piece[][] boardCopy;
+		
+		while (--col > 0 && --row > 0) {
+			if (board[col][row] == null) {
+				boardCopy = getCopyOfBoard(board);
+				
+				boardCopy[col][row] = piece;
+				boardCopy[piece.getCol()][piece.getRow()] = null;
+				
+				if (isKingSafe(kingPosition, adversaryPieces, boardCopy)) {
+					moves.add(new Point(col, row));
+				}
+			} else {
+				if (board[col][row].getTeam() != piece.getTeam()) {
+					boardCopy = getCopyOfBoard(board);
+					
+					boardCopy[col][row] = piece;
+					boardCopy[piece.getCol()][piece.getRow()] = null;
+					
+					if (isKingSafe(kingPosition, adversaryPieces, boardCopy)) {
+						moves.add(new Point(col, row));
+					}
+				}
+				break;
+			}
+		}
+		
+		return moves;
+	}
+	
+	
+	public static boolean isKingSafe (final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
+		
+		for (final Piece adversaryPiece : adversaryPieces) {
+			if (adversaryPiece.checkValidMove(kingPosition, board)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	public static List<Point> getDiagonalDownRightMoves (Piece piece, final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
+		
+		final List<Point> moves = new ArrayList<>();
+		
+		int col = piece.getCol();
+		int row = piece.getRow();
+		
+		Piece[][] boardCopy;
+		
+		while (++col < 7 && ++row < 7) {
+			if (board[col][row] == null) {
+				boardCopy = getCopyOfBoard(board);
+				
+				boardCopy[col][row] = piece;
+				boardCopy[piece.getCol()][piece.getRow()] = null;
+				
+				if (isKingSafe(kingPosition, adversaryPieces, boardCopy)) {
+					moves.add(new Point(col, row));
+				}
+			} else {
+				if (board[col][row].getTeam() != piece.getTeam()) {
+					boardCopy = getCopyOfBoard(board);
+					
+					boardCopy[col][row] = piece;
+					boardCopy[piece.getCol()][piece.getRow()] = null;
+					
+					if (isKingSafe(kingPosition, adversaryPieces, boardCopy)) {
+						moves.add(new Point(col, row));
+					}
+				}
+				break;
+			}
+		}
+		
+		return moves;
+	}
+	
+	
+	public static List<Point> getDiagonalDownLeftMoves (Piece piece, final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
+		
+		final List<Point> moves = new ArrayList<>();
+		
+		int col = piece.getCol();
+		int row = piece.getRow();
+		
+		Piece[][] boardCopy;
+		
+		while (--col > 0 && ++row < 7) {
+			if (board[col][row] == null) {
+				boardCopy = getCopyOfBoard(board);
+				
+				boardCopy[col][row] = piece;
+				boardCopy[piece.getCol()][piece.getRow()] = null;
+				
+				if (isKingSafe(kingPosition, adversaryPieces, boardCopy)) {
+					moves.add(new Point(col, row));
+				}
+			} else {
+				if (board[col][row].getTeam() != piece.getTeam()) {
+					boardCopy = getCopyOfBoard(board);
+					
+					boardCopy[col][row] = piece;
+					boardCopy[piece.getCol()][piece.getRow()] = null;
+					
+					if (isKingSafe(kingPosition, adversaryPieces, boardCopy)) {
+						moves.add(new Point(col, row));
+					}
+				}
+				break;
+			}
+		}
+		
+		return moves;
 	}
 	
 }
