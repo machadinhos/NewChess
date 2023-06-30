c15e211270e3dc0080782cb09c6c7c69e5f415a6
machadinhos <pbmachado2002@gmail.com>
2023-06-30 22:30:00 +0100

    Coded Rook methods

--- src/pieces/picestypes/Rook.java
+++ src/pieces/picestypes/Rook.java
@@ -1,12 +1,16 @@
 package pieces.picestypes;
 
 import pieces.Piece;
+import pieces.PiecesUtils;
 import pieces.Team;
 
 import java.awt.*;
 import java.util.List;
 
 public class Rook extends Piece {
+	
+	private boolean hasMoved;
+	
 	
 	public Rook (final Team team, final int col, final int row) {
 		
@@ -15,9 +19,17 @@ public class Rook extends Piece {
 	
 	
 	@Override
+	public void move (int col, int row, Piece[][] board, List<Piece> adversaryPieces) {
+		
+		super.move(col, row, board, adversaryPieces);
+		this.hasMoved = true;
+	}
+	
+	
+	@Override
 	public List<Point> getValidMoves (final Point kingPosition, final List<Piece> adversaryPieces, final Piece[][] board) {
 		
-		return null;
+		return PiecesUtils.getValidCardinalMoves(this, kingPosition, adversaryPieces, board);
 	}
 	
 	
