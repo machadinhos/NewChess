9dbe6bb0b4ad92b4b5e1a72b221d1328b66b2209
machadinhos <pbmachado2002@gmail.com>
2023-07-01 20:36:41 +0100

    Coded isMoveValid Pawn

--- src/pieces/picestypes/Pawn.java
+++ src/pieces/picestypes/Pawn.java
@@ -8,9 +8,26 @@ import java.util.List;
 
 public class Pawn extends Piece {
 	
+	private boolean hasNotMoved = true;
+	
+	
 	public Pawn (final Team team, final int col, final int row) {
 		
 		super(team, col, row);
+	}
+	
+	
+	public boolean hasNotMoved () {
+		
+		return hasNotMoved;
+	}
+	
+	
+	@Override
+	public void move (int col, int row, Piece[][] board, List<Piece> adversaryPieces) {
+		
+		super.move(col, row, board, adversaryPieces);
+		this.hasNotMoved = false;
 	}
 	
 	
@@ -24,7 +41,39 @@ public class Pawn extends Piece {
 	@Override
 	public boolean isMoveValid (final int col, final int row, final Piece[][] board) {
 		
+		boolean valid = false;
+		
+		if (super.getTeam() == Team.WHITE) {
+			
+			if (super.getCol() == col && super.getRow() - 2 == row) {
+				
+				valid = true;
+			} else if (super.getCol() == col && super.getRow() - 1 == row) {
+				
+				valid = true;
+			} else if (Math.abs(super.getCol() - col) == 1 && super.getRow() - 1 == row) {
+				
+				valid = true;
+			}
+		} else {
+			
+			if (super.getCol() == col && super.getRow() + 2 == row) {
+				
+				valid = true;
+			} else if (super.getCol() == col && super.getRow() + 1 == row) {
+				
+				valid = true;
+			} else if (Math.abs(super.getCol() - col) == 1 && super.getRow() + 1 == row) {
+				
+				valid = true;
+			}
+		}
+		
+		if (valid) {
+			
+			return board[col][row] == null || board[col][row].getTeam() != super.getTeam();
+		}
+		
 		return false;
 	}
-	
 }
