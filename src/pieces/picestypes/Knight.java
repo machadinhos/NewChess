f7a7c5670debf7de50d2390038ff8d1659d4ca74
machadinhos <pbmachado2002@gmail.com>
2023-07-01 17:37:38 +0100

    Coded isMoveValid Knight

--- src/pieces/picestypes/Knight.java
+++ src/pieces/picestypes/Knight.java
@@ -24,6 +24,21 @@ public class Knight extends Piece {
 	@Override
 	public boolean isMoveValid (final int col, final int row, final Piece[][] board) {
 		
+		boolean valid = false;
+		
+		if (Math.abs(super.getCol() - col) == 2 && Math.abs(super.getRow() - row) == 1) {
+			
+			valid = true;
+		} else if (Math.abs(super.getCol() - col) == 1 && Math.abs(super.getRow() - row) == 2) {
+			
+			valid = true;
+		}
+		
+		if (valid) {
+			
+			return board[row][col] == null || board[row][col].getTeam() != super.getTeam();
+		}
+		
 		return false;
 	}
 	
